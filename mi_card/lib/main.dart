43a21edb216f0c20b74a5368a3d908c0f4966dcd
Mihai Georgevici <mihai@titlecapture.com>
2022-01-22 14:36:32 +0200

    Created the mi card app

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -1,13 +1,127 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundColor: Colors.white,
+                backgroundImage: AssetImage('assets/images/avatar.jpg'),
+              ),
+              Text(
+                'Ioan Mihai Georgevici',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'Pacifico',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30.0,
+                ),
+              ),
+              Text(
+                'SOFTWARE DEVELOPER',
+                style: TextStyle(
+                  color: Colors.teal[100],
+                  fontFamily: 'Source Sans Pro',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18.0,
+                  letterSpacing: 1.5,
+                ),
+              ),
+              SizedBox(
+                height: 30.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+40725-111-222',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'georgevici.ioan@gmail.com',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              // Card(
+              //   color: Colors.white,
+              //   margin: EdgeInsets.symmetric(
+              //     vertical: 10.0,
+              //     horizontal: 25.0,
+              //   ),
+              //   child: Padding(
+              //     padding: const EdgeInsets.all(10.0),
+              //     child: Row(
+              //       children: <Widget>[
+              //         Icon(
+              //           Icons.email,
+              //           color: Colors.teal,
+              //         ),
+              //         SizedBox(
+              //           width: 10.0,
+              //         ),
+              //         Text(
+              //           'georgevici.ioan@gmail.com',
+              //           style: TextStyle(
+              //             color: Colors.teal.shade900,
+              //             fontFamily: 'Source Sans Pro',
+              //             fontSize: 20.0,
+              //           ),
+              //         ),
+              //       ],
+              //     ),
+              //   ),
+              // ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
