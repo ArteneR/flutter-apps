00bf76a8c93bdd8628d038beb2c20356bef9568c
Mihai Georgevici <mihai@titlecapture.com>
2022-01-23 15:59:33 +0200

    Completed the Dicee app

--- dicee/lib/main.dart
+++ dicee/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
@@ -15,9 +17,53 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var leftDiceNumber = 1;
+  var rightDiceNumber = 4;
+
+  void changeDiceFace() {
+    leftDiceNumber = Random().nextInt(6) + 1;
+    rightDiceNumber = Random().nextInt(6) + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                child: Image.asset('assets/images/dice$leftDiceNumber.png'),
+                onPressed: () {
+                  setState(() {
+                    changeDiceFace();
+                  });
+                },
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                child: Image.asset('assets/images/dice$rightDiceNumber.png'),
+                onPressed: () {
+                  setState(() {
+                    changeDiceFace();
+                  });
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
